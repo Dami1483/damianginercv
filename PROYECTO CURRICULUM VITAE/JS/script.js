d235b387c6590b4f2bb39516027d57dd02d11646
Dami1483 <95728856+Dami1483@users.noreply.github.com>
2023-03-07 23:08:06 -0300

    Add files via upload

---
+++ PROYECTO CURRICULUM VITAE/JS/script.js
@@ -0,0 +1,27 @@
+function myFunction () {
+    alert("Título obtenido: Técnico en Electrónica");
+}
+
+function myFunction2 () {
+    alert("Técnico en Refrigeración. Reparacion de heladeras y aires acondicionados.");
+}
+
+function myFunction3 () {
+    alert("INSTALACIÓN, REPARACIÓN Y MANTENIMIENTO DE MÁQUINAS DE CAFÉ SOLUBLE Y EN GRANO.");
+}
+
+function myFunction4 () {
+    alert("REPARANDO NOTEBOOKS Y DESKTOP DE LA LÍNEA VAIO, CÁMARAS DIGITALES Y REFLEX DE LA LÍNEA CYBERSHOT Y ALPHA Y CELULARES LÍNEA SONY ERICSSON.");
+}
+
+function myFunction5 () {
+    alert("REPARACIÓN IMPRESORAS HEWLETT PACKARD (LÁSER Y DESKJET), REPARACIÓN DE PC DE ESCRITORIO Y NOTEBOOKS, TAMBIÉN HP Y COMPAQ.");
+}
+
+function myFunction6 () {
+    alert("REPARANDO IMPRESORAS LÁSER Y DESKJET, TESTEO Y REPARACIÓN DE ESCÁNERS Y REMANUFACTURA Y TESTEO DE PC Y NOTEBOOKS PARA HP");
+}
+
+function myFunction7 () {
+    alert("REPARACIÓN DE IMPRESORAS LÁSER Y DESKJET, TESTEO DE MOTHERBORDS DE PC Y ESCÁNERS.");
+}
